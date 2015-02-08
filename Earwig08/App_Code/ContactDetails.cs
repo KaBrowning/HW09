4adf57a9a35edd671f315ecdf7991c8ec071d18a
Kathryn Browning <kbrowni3@my.westga.edu>
2015-02-08 09:02:33 -0500

    Added ContactDetails.cs

---
+++ Earwig08/App_Code/ContactDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ContactDetails
+/// </summary>
+public class ContactDetails
+{
+	public ContactDetails()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
