e80051406f40f7e733bf512cb4fe376126f27dc0
Kathryn Browning <kbrowni3@my.westga.edu>
2015-02-08 09:33:45 -0500

    Added all of the necessary variables in contactdetails.cs that will allow the program to store contact info

--- Earwig08/App_Code/ContactDetails.cs
+++ Earwig08/App_Code/ContactDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ using System.Web;
 /// </summary>
 public class ContactDetails
 {
+    private string _firstName;
+    private string _lastName;
+    private string _email;
+    private string _phone;
+
     public static ContactDetails GetContactDetails()
     {
         var cart = HttpContext.Current.Session["Contact"] as ContactDetails;
@@ -16,4 +22,43 @@ public class ContactDetails
         return (ContactDetails)HttpContext.Current.Session["Contact"];
     }
 
+    public string FirstName
+    {
+        get { return this._firstName; }
+        set
+        {
+            Trace.Assert(value != null, "Invalid First Name");
+            this._firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this._lastName; }
+        set
+        {
+            Trace.Assert(value != null, "Invalid Last Name");
+            this._lastName = value;
+        }
+    }
+
+    public string Email
+    {
+        get { return this._email; }
+        set
+        {
+            Trace.Assert(value != null, "Invalid Email");
+            this._email = value;
+        }
+    }
+
+    public string Phone
+    {
+        get { return this._phone; }
+        set
+        {
+            Trace.Assert(value != null, "Invalid Phone Number");
+            this._phone = value;
+        }
+    }
 }
